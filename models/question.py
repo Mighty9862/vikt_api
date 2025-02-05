c736b0556896f8c525ab5a31478fcd4ba3c54f42
Никита Рожков <admin@admin.local>
2025-02-05 17:25:41 +0300

    Upgrade questhions column len

--- models/question.py
+++ models/question.py
@@ -6,9 +6,9 @@ from datetime import datetime
 class Question(Base):
     __tablename__ = "questions"
     
-    question: Mapped[str] = mapped_column(String(255), nullable=False, unique=False)
-    answer: Mapped[str] = mapped_column(String(255), nullable=True, unique=False)
-    chapter: Mapped[str] = mapped_column(String(255), nullable=False, unique=False)
+    question: Mapped[str] = mapped_column(String(1000), nullable=False, unique=False)
+    answer: Mapped[str] = mapped_column(String(1000), nullable=True, unique=False)
+    chapter: Mapped[str] = mapped_column(String(1000), nullable=False, unique=False)
     
     #profile: Mapped["Profile"] = relationship(back_populates="user", cascade="all, delete-orphan", uselist=False)
     #settings: Mapped["SettingsModel"] = relationship(back_populates="user", cascade="all, delete-orphan", uselist=False)
